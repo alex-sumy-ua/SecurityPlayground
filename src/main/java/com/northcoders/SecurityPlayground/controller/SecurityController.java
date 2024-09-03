c9de585e58e5ae9451fbb664153dee092c9f180c
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2024-09-03 15:01:06 +0100

    feat: task 5 protected greeting now returns username and github username

--- src/main/java/com/northcoders/SecurityPlayground/controller/SecurityController.java
+++ src/main/java/com/northcoders/SecurityPlayground/controller/SecurityController.java
@@ -2,6 +2,8 @@ package com.northcoders.SecurityPlayground.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +18,16 @@ public class SecurityController {
         return "Greetings, distant user.";
     }
 
+//    @GetMapping("/protected/greeting")    // usage: http://localhost:8082/api/v1/protected/greeting
+//    public static String getProtectedGreeting() {
+//        return "Hello special people!";
+//    }
+
     @GetMapping("/protected/greeting")    // usage: http://localhost:8082/api/v1/protected/greeting
-    public static String getProtectedGreeting() {
-        return "Hello special people!";
+    public static ResponseEntity<String> getProtectedGreeting(@AuthenticationPrincipal OAuth2User user) {
+        String username = user.getAttribute("name");
+        String githubUsername = user.getAttribute("login");
+        return new ResponseEntity<>("Hey " + username + " " + githubUsername + "!", HttpStatus.OK);
     }
 
 }
