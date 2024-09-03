278f12b7cfb9d769efe70ec069e038181816510d
alex-sumy-ua <plachkovskyy@yahoo.com>
2024-09-03 11:20:09 +0100

    feat: Task 2: A Warm-ish Welcome - completed

---
+++ src/main/java/com/northcoders/SecurityPlayground/controller/SecurityController.java
@@ -0,0 +1,27 @@
+package com.northcoders.SecurityPlayground.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1") // Use: localhost:8082/api/v1/jokes
+public class SecurityController {
+
+
+    @GetMapping("/open/greeting")    // usage: http://localhost:8082/api/v1/open/greeting
+    public static String getOpenGreeting() {
+        return "Hello everyone!";
+    }
+
+    @GetMapping("/protected/greeting")    // usage: http://localhost:8082/api/v1/protected/greeting
+    public static String getProtectedGreeting() {
+        return "Hello special people!";
+    }
+
+
+
+}
